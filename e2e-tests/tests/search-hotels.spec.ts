a21274670c898458dc3bf9a7c57467ff5105fe7f
Robert Leung <robert.leung1@outlook.com>
2024-03-27 21:11:50 -0400

    added hotel detail page and e2e test

--- e2e-tests/tests/search-hotels.spec.ts
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -26,3 +26,11 @@ test("Should show hotel search results", async ({ page }) => {
   await expect(page.getByText("Hotels found in Dublin")).toBeVisible();
   await expect(page.getByText("Dublin Getaways")).toBeVisible();
 });
+
+test("should show hotel detail", async ({ page }) => {
+  await page.getByPlaceholder("Where are you going?").fill("Dublin");
+  await page.getByRole("button", { name: "Search" }).click();
+  await page.getByText("Dublin Getaways").click();
+  await expect(page).toHaveURL(/detail/);
+  await expect(page.getByRole("button", { name: "Book now" })).toBeVisible();
+});
--- frontend/src/pages/Detail.tsx
+++ frontend/src/pages/Detail.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import * as apiClient from "./../api-client";
 import { AiFillStar } from "react-icons/ai";
+import GuestInfoForm from "../forms/GuestInfoFrom/GuestInfoForm";
 
 const Detail = () => {
   const { hotelId } = useParams();
@@ -47,7 +48,12 @@ const Detail = () => {
 
       <div className="grid grid-cols-1 lg:grid-cols-[2fr_1fr]">
         <div className="whitespace-pre-line">{hotel.description}</div>
-        <div className="h-fit">{/* <GuestInfo></GuestInfo> */}</div>
+        <div className="h-fit">
+          <GuestInfoForm
+            pricePerNight={hotel.pricePerNight}
+            hotelId={hotel._id}
+          ></GuestInfoForm>
+        </div>
       </div>
     </div>
   );
