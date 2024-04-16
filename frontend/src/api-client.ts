a60e0eaf0740d65ee4d819eeb9ef90ea0c709df4
Robert Leung <robert.leung1@outlook.com>
2024-04-15 21:40:34 -0400

    added my bookings page and test

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -212,3 +212,13 @@ export const createRoomBooking = async (formData: BookingFormData) => {
     throw new Error("Error booking room");
   }
 };
+
+export const fetchMyBookings = async () => {
+  const response = await fetch(`${API_BASE_URL}/api/my-bookings`, {
+    credentials: "include",
+  });
+  if (!response.ok) {
+    throw new Error("Unable to fetch bookings");
+  }
+  return response.json();
+};
